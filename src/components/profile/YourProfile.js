0031726393a00517f95e5d35c0ea864609e6460b
Danny Herrmann <dannyherrmann91@gmail.com>
2022-12-16 14:22:30 -0600

    firebase photo storage

--- src/components/profile/YourProfile.js
+++ src/components/profile/YourProfile.js
@@ -1,19 +1,32 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
+import { useState } from "react"
+import { photoStorage } from "./PhotoStorage"
+
 export const YourProfile = () => {
+
+  const troupeUser = localStorage.getItem("troupe_user");
+  const troupeUserObject = JSON.parse(troupeUser)
+  const [image, setImage] = useState(null)
+  const [imageUrl, setImageUrl] = useState(null)
+
+    // Handles selecting an image
+    const handleChange = (event) => {
+      if (event.target.files[0]) {
+        setImage(event.target.files[0]);
+      }
+    };
+
+      // Handles calling the upload image function
+  const handleUpload = () => {
+    photoStorage.upload("images", image).then((photoObject) => {
+      // Returns image object, you will want to add these properties
+      // to an object in your database
+      // EX: a user if it's a profile picture
+      setImageUrl(photoObject.downloadURL);
+    });
+  };
+
     return (
+      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 pt-8">
       <form className="space-y-6" action="#" method="POST">
         <div className="bg-white px-4 py-5 shadow sm:rounded-lg sm:p-6">
           <div className="md:grid md:grid-cols-3 md:gap-6">
@@ -65,13 +78,17 @@ export const YourProfile = () => {
                 <label className="block text-sm font-medium text-gray-700">Photo</label>
                 <div className="mt-1 flex items-center space-x-5">
                   <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-                    <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                    </svg>
+                  <img
+                          className="h-8 w-8 rounded-full"
+                          src={troupeUserObject.userPhoto}
+                          alt=""
+                        />
                   </span>
+                  <input type="file" onChange={handleChange} />
                   <button
                     type="button"
                     className="rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    onClick={handleUpload}
                   >
                     Change
                   </button>
@@ -354,6 +371,7 @@ export const YourProfile = () => {
           </button>
         </div>
       </form>
+      </div>
     )
   }
   
