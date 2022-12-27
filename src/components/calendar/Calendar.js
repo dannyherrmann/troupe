a69ac3b7b3201d17ba1e06103e3b6987f76eeced
Danny <dannyherrmann91@gmail.com>
2022-12-26 21:19:43 -0600

    calendar work

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,7 +1,8 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
 import { Menu, Transition } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/24/outline'
+import { eachDayOfInterval, endOfMonth, format, startOfMonth, startOfToday, endOfWeek, isToday, isSameMonth, isEqual, parse, add, startOfWeek } from "date-fns";
 
 export const Calendar = () => {
 
@@ -66,14 +67,40 @@ const meetings = [
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
+
+const today = startOfToday()
+
+const [selectedDay, setSelectedDay] = useState(today)
+const [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'))
+const firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
+
+const newDays = eachDayOfInterval({
+  start: startOfWeek(firstDayCurrentMonth),
+  end: endOfWeek(endOfMonth(firstDayCurrentMonth)),
+})
+
+const previousMonth = () => {
+  let firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 })
+  setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
+}
+
+const nextMonth = () => {
+  let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 })
+  setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
+}
+
   return (
     <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
       <div className="md:pr-14">
         <div className="flex items-center">
-          <h2 className="flex-auto font-semibold text-gray-900">January 2022</h2>
+          <h2 className="flex-auto font-semibold text-gray-900">
+            {/* January 2022 */}
+            {format(firstDayCurrentMonth, 'MMMM yyyy')}
+            </h2>
           <button
             type="button"
             className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+            onClick={previousMonth}
           >
             <span className="sr-only">Previous month</span>
             <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
@@ -81,38 +108,42 @@ function classNames(...classes) {
           <button
             type="button"
             className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+            onClick={nextMonth}
           >
             <span className="sr-only">Next month</span>
             <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
           </button>
         </div>
         <div className="mt-10 grid grid-cols-7 text-center text-xs leading-6 text-gray-500">
+          <div>S</div>
           <div>M</div>
           <div>T</div>
           <div>W</div>
           <div>T</div>
           <div>F</div>
           <div>S</div>
-          <div>S</div>
         </div>
         <div className="mt-2 grid grid-cols-7 text-sm">
-          {days.map((day, dayIdx) => (
-            <div key={day.date} className={classNames(dayIdx > 6 && 'border-t border-gray-200', 'py-2')}>
+          {newDays.map((day, dayIdx) => (
+            <div key={day.toString()} className={classNames(dayIdx > 6 && 'border-t border-gray-200', 'py-2')}>
               <button
                 type="button"
+                onClick={() => setSelectedDay(day)}
                 className={classNames(
-                  day.isSelected && 'text-white',
-                  !day.isSelected && day.isToday && 'text-indigo-600',
-                  !day.isSelected && !day.isToday && day.isCurrentMonth && 'text-gray-900',
-                  !day.isSelected && !day.isToday && !day.isCurrentMonth && 'text-gray-400',
-                  day.isSelected && day.isToday && 'bg-indigo-600',
-                  day.isSelected && !day.isToday && 'bg-gray-900',
-                  !day.isSelected && 'hover:bg-gray-200',
-                  (day.isSelected || day.isToday) && 'font-semibold',
+                  isEqual(day, selectedDay) && 'text-white',
+                  !isEqual(day, selectedDay) && isToday(day) && 'text-indigo-600',
+                  !isEqual(day, selectedDay) && !isToday(day) && isSameMonth(day, firstDayCurrentMonth) && 'text-gray-900',
+                  !isEqual(day, selectedDay) && !isToday(day) && !isSameMonth(day, firstDayCurrentMonth) && 'text-gray-400',
+                  isEqual(day, selectedDay) && isToday(day) && 'bg-indigo-600',
+                  isEqual(day, selectedDay) && !isToday(day) && 'bg-gray-900',
+                  !isEqual(day, selectedDay) && 'hover:bg-gray-300',
+                  (day.isSelected || isToday(day)) && 'font-semibold',
                   'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
                 )}
               >
-                <time dateTime={day.date}>{day.date.split('-').pop().replace(/^0/, '')}</time>
+                <time dateTime={format(day, 'yyyy-MM-dd')}>
+                  {format(day, 'd')}
+                </time>
               </button>
             </div>
           ))}
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -78,6 +78,12 @@ export const NavBar = ({setOpenNewEvent}) => {
                     >
                         Team
                     </NavLink>
+                    <NavLink to="/calendar"
+                    onClick={() => setSelectedTab(3)}
+                    className={selectedTab === 3 ? "inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900" : "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"}
+                    >
+                        Calendar
+                    </NavLink>
                 </div>
               </div>
               <div className="flex items-center">
